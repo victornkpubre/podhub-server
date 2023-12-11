3b12eb199019b0e09b6af00218bd3a27abbdbe75
Victor Nkpubre <victornkpubre@gmail.com>
2023-12-11 10:03:06 +0100

    Body Parser Update

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,21 @@ import playlistRouter from '#/routers/playlist'
 import profileRouter from '#/routers/profile'
 import historyRouter from '#/routers/history'
 import { errorHandler } from "./middleware/errors";
+import bodyParser from "body-parser"
 
-const path = require('path')
 const app = express();
 const PORT = 8989;
-
-var bodyParser = require("body-parser");
-app.use(bodyParser.json({limit: '100kb'}));
-
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 app.use(express.static('src/public'));
+
+
+app.use(bodyParser.json({limit: '150mb'}));
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+limit: '150mb',
+extended: true
+})); 
 
 
 app.use("/auth", authRouter);
